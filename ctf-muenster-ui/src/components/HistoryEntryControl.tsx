bb8289db3fb652e5a388529bd13869a80b6192f4
Lennard Bacher <lennard.bacher@t-online.de>
2022-09-23 17:49:26 +0200

    History

--- ctf-muenster-ui/src/components/HistoryEntryControl.tsx
+++ ctf-muenster-ui/src/components/HistoryEntryControl.tsx
@@ -6,11 +6,30 @@ export interface HistoryEntryProps {
 }
 
 const HistoryEntryControl: React.FC<HistoryEntryProps> = () => {
+
+  let date = new Date();
+
   return (
     <div className="historyEntry">
-      <div>Flag name</div>
-      <div>User</div>
-      <div>{new Date().toUTCString()}</div>
+      <div style={{display: 'flex'}}>
+        <div className='flagIcon'></div>
+        <div className='flagName'>Flag name</div>
+        <div className='captured' style={{flex: 1, textAlign: 'end', marginTop: 5}}>Captured!</div>
+      </div>
+      
+      <div style={{display: 'flex'}}>
+        
+        <div className='points'>50</div>
+        <div style={{marginTop: '12px', marginLeft: '3px'}}>Punkte</div>
+        <div style={{display: 'flex', justifyContent: 'end', flex: 1, marginTop: '10px'}}>
+          <div className='flagMiniIcon'></div>
+          <div className='dateTime'>
+            {date.getDay() + '.' + date.getMonth() + '.' + date.getFullYear()}
+          </div>
+        </div>
+      </div>
+     
+      
     </div>
   );
 };
